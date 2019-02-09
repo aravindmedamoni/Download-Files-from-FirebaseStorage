b1e4609eb42c1d647d3fbcdaf8f2e830283beff8
aravindmedamoni <aravnd.accurate@gmail.com>
2019-02-09 19:06:27 +0530

    This is my fifth commit

--- app code/java/com/aravind/downloadfilesfromfirebasestorage/MainActivity.java
+++ app code/java/com/aravind/downloadfilesfromfirebasestorage/MainActivity.java
@@ -31,37 +31,40 @@ public class MainActivity extends AppCompatActivity {
         download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dowload();
+                dowload("javascript_the_good_parts.pdf");
             }
         });
     }
 
-    public void dowload() {
+    public void dowload(final String filename) {
         storageReference=firebaseStorage.getInstance().getReference();
-        ref=storageReference.child("the _book_of_css.pdf");
+        ref=storageReference.child(filename);
+      //  final String firstFilename="the _book_of_css";
 
         ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
                 String url=uri.toString();
-                downloadFile(MainActivity.this,"the _book_of_css",".pdf",DIRECTORY_DOWNLOADS,url);
-                Toast.makeText(MainActivity.this, "downloaded", Toast.LENGTH_SHORT).show();
+               // downloadFile(MainActivity.this,firstFilename,".pdf",DIRECTORY_DOWNLOADS,url);
+                downloadFile(MainActivity.this,filename,DIRECTORY_DOWNLOADS,url);
+                Toast.makeText(MainActivity.this, "downloading", Toast.LENGTH_SHORT).show();
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(MainActivity.this, "sorry", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    public void downloadFile(Context context,String filename,String fileExtension,String destinationDirectory,String url){
-
+   // public void downloadFile(Context context,String filename,String fileExtension,String destinationDirectory,String url){
+public void downloadFile(Context context,String filename,String destinationDirectory,String url){
         DownloadManager downloadManager=(DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
         Uri uri=Uri.parse(url);
         DownloadManager.Request request=new DownloadManager.Request(uri);
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setDestinationInExternalFilesDir(context,destinationDirectory,filename+fileExtension);
+      //  request.setDestinationInExternalFilesDir(context,destinationDirectory,filename+fileExtension);
+        request.setDestinationInExternalFilesDir(context,destinationDirectory,filename);
         downloadManager.enqueue(request);
     }
 }
